f717a5ccc11018dda89e8875108ebb16b3dc5ae0
ahmedzahid121 <zahid.poolme@gmail.com>
2024-05-14 21:23:45 +1200

    Google Map is working

--- Pool/components/HomeMap/Map.tsx
+++ Pool/components/HomeMap/Map.tsx
@@ -1,25 +1,35 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'
 
 const HomeMap = () => {
+
+  const initialRegion = {
+  latitude: -36.8485,
+  longitude: 174.7633,
+  latitudeDelta: 0.1,
+  longitudeDelta: 0.1
+  };
+
   return (
-    <View style={{height: 300, backgroundColor:"#a0abff", justifyContent: 'center',
-    alignItems: 'center'
-    
-    }}>
-        <MapView
-          style={{height: 100, width:100}}
-          provider={PROVIDER_GOOGLE}
-          initialRegion={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-            }}
-        />
+    <View style={{ height: 300, backgroundColor: "#a0abff", justifyContent: 'center', alignItems: 'center' }}>
+      <MapView style={styles.map}
+       
+        initialRegion={initialRegion}/>
     </View>
-  )
-}
+  );
+};
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: '100%',
+    height: '100%',
+    
+  },
+});
 
 export default HomeMap
