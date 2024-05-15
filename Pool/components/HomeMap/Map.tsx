e524f535129bf039933bbb29ffd84a82ae582cbe
ahmedzahid121 <zahid.poolme@gmail.com>
2024-05-15 20:59:47 +1200

    Customer and driver Marker is added

--- Pool/components/HomeMap/Map.tsx
+++ Pool/components/HomeMap/Map.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'
+import { View, Text, StyleSheet, Image } from 'react-native'
+import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps'
 
 const HomeMap = () => {
 
@@ -15,10 +15,49 @@ const HomeMap = () => {
     <View >
       <MapView style={styles.map}
        
-        initialRegion={initialRegion}/>
-    </View>
+        initialRegion={initialRegion}
+        >
+          <Marker
+            coordinate={{ latitude: -36.8485, longitude: 174.7633 }}
+            title="Driver Marker"
+            description="Some description" 
+          >
+            <Image
+          source={require('../../assets/images/Cars/CarMarker.png')}
+          style={styles.DriverMarker}/>
+          </Marker>
+
+
+
+          <Marker
+            coordinate={{ latitude: -36.73126161975735,  longitude: 174.7285423446079 }}
+            title="My Marker"
+            description="Some description"
+            
+            
+          >
+
+          <Image
+          source={require('../../assets/images/Cars/Marker.png')}
+          style={styles.CustomerMarker}/>
+          </Marker>
+
+
+
+          
+
+      </MapView>
+         
+         
+          
+     
+    </View> 
   );
 };
+
+
+
+
 
 
 const styles = StyleSheet.create({
@@ -30,8 +69,19 @@ const styles = StyleSheet.create({
     height: '100%',
     
   },
+
+  DriverMarker: {
+    width: 50,
+    height: 50,
+  },
+  CustomerMarker: {
+    width: 50,
+    height: 50,
+  },
 });
 
 export default HomeMap
 
-//style={{ height: 300, backgroundColor: "#a0abff", justifyContent: 'center', alignItems: 'center' }}
+//Use Flatlist to render real life data of the drivers and the Customer from the Back-end
+
+
